b04f7e63e625d220b24f92f70485fe62c3996e69
german sanchez <andres_motta14@hotmail.com>
2019-09-29 21:28:47 -0500

    Se cambia apuntador de proyecto en el server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ade-metering'));
+app.use(express.static('./dist/angular-pro-sidebar'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname, '/dist/ade-metering/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/angular-pro-sidebar/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
