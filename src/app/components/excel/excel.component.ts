6273b46587d81ab37533788af1f2a3ccbb31c68c
andres <german.sanchez@prodigious.com>
2020-06-20 13:39:27 -0500

    table

--- src/app/components/excel/excel.component.ts
+++ src/app/components/excel/excel.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '../../../../node_modules/@angular/common/http';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-excel',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExcelComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('TABLE', { static: false }) TABLE: ElementRef;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getData();
   }
+
+  getData() {
+    this.http.get('https://tesis-node-mysql.herokuapp.com/registers')
+    .subscribe((data: any) => {
+      console.log(data)
+    })
+  }
+
+  ExportTOExcel() {  
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.TABLE.nativeElement);  
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Resultados');
+    XLSX.writeFile(wb, 'datos.xlsx');  
+  }  
 
 }
